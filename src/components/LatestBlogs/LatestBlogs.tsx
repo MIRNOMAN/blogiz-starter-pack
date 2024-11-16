6ab0a4d31db81ee500812fe59e176ca5907d36e4
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-16 18:27:18 +0600

    create latestblogs

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,11 @@
+
+const LatestBlogs = () => {
+  return (
+    <div>
+        <h1 className="text-4xl text-center my-4">Latest Blogs From <span className="text-accent">Blogiz</span></h1>
+        <p className="text-xl text-center w-2/4 mx-auto"><i>Dive into the fascinating world of quantum computing, where unlocking unprecedented computational power.</i></p>
+    </div>
+  )
+}
+
+export default LatestBlogs
