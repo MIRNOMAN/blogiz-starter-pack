c94cfdbb6efa05c827156f0fcb9b19f28ccad8ca
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-16 23:04:02 +0600

    create blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+
+
+const AllBlogsPage = () => {
+  return (
+    <div>
+         <h1 className="text-4xl text-center my-4">All Blogs From <span className="text-accent">Blogiz</span></h1>
+         <p className="text-xl text-center w-2/4 mx-auto"><i>Dive into the fascinating world of quantum computing, where unlocking unprecedented computational power.</i></p>
+    </div>
+  )
+}
+
+export default AllBlogsPage
