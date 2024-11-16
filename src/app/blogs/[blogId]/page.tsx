e27b45ee798e7653263da070e62870c0b161a93a
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-17 00:47:08 +0600

    state ssg generate

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -7,6 +7,14 @@ interface BlogId{
     }
 }
 
+export const generateStaticParams = async ()=>{
+    const res = await fetch("http://localhost:5000/blogs");
+    const blogs = await res.json();
+    return blogs.slice(0,3).map((blog:Blog) =>({
+        blogId:blog.id
+    }))
+}
+
 const BlogDetailsPage = async({params}:BlogId) => {
     const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`,{
         cache: "no-store",
